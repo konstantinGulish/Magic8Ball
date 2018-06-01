c5e38b4ab5ae6c06006af25196b01d477a64cd92
konstantinGulish <konstantin_gulish@yahoo.com>
2018-06-01 14:14:16 -0400

    added comments to improve readability

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 import java.util.Random;
 public class Magic8Ball {
     public static void main (String [] args) {
+
+        // Initiate scanner and random
         Scanner in = new Scanner (System.in);
         Random rnd = new Random();
+
+        // Add all of the potential answers to the array
         String [] answers = {"It is certain",
                               "It is decidedly so",
                               "Without a doubt",
@@ -25,12 +29,13 @@ public class Magic8Ball {
                               "Outlook not so good",
                               " Very doubtful"
                         };
-
+        // Initialize the variables
         boolean moreQuestions = true;
         String question = "";
         String answer = "";
         String reply = "";
 
+        // Keep asking the user questions until the user enters "n" or "N"
         while (moreQuestions) {
             System.out.print("Enter your question: ");
             question = in.nextLine();
@@ -41,6 +46,8 @@ public class Magic8Ball {
             reply = in.nextLine();
             if (reply.equals("n") || reply.equals ("N")) moreQuestions = false;
         }
+
+        // Close out the scanner to release resources
         in.close();
     }
 
