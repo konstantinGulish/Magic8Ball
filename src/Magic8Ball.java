92e791129a674730e1d2aecad903ad6e66077609
konstantinGulish <konstantin_gulish@yahoo.com>
2018-06-01 15:11:50 -0400

    refactored the program to use methods

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -8,7 +8,40 @@ public class Magic8Ball {
         Random rnd = new Random();
 
         // Add all of the potential answers to the array
-        String [] answers = {"It is certain",
+        String [] answers = getAnswers();
+
+        // Initialize the variables
+        boolean moreQuestions = true;
+        String question = "";
+        String answer = "";
+        String reply = "";
+
+        // Keep asking the user questions until the user enters "n" or "N"
+        shake8Ball(in, rnd, answers, moreQuestions);
+
+
+    }
+    private static String pickRandomAnswer (String[] answers, Random rnd) {
+        return answers[rnd.nextInt(20)];
+    }
+    private static void shake8Ball(Scanner in,  Random rnd, String[] answers, boolean moreQuestions) {
+        String question;
+
+        String reply;
+        while (moreQuestions) {
+            System.out.print("Enter your question: ");
+            question = in.nextLine();
+            String answer = pickRandomAnswer(answers, rnd);
+            System.out.println("YOU ASKED: " + question);
+            System.out.println("MAGIC 8-BALL SAYS: " + answer);
+            System.out.print("Do you have another question for the Magic 8-Ball? (y/n) ");
+            reply = in.nextLine();
+            if (reply.equals("n") || reply.equals ("N")) moreQuestions = false;
+        }
+    }
+
+    private static String[] getAnswers() {
+        return new String[]{"It is certain",
                               "It is decidedly so",
                               "Without a doubt",
                               "Yes definitely",
@@ -29,26 +62,6 @@ public class Magic8Ball {
                               "Outlook not so good",
                               " Very doubtful"
                         };
-        // Initialize the variables
-        boolean moreQuestions = true;
-        String question = "";
-        String answer = "";
-        String reply = "";
-
-        // Keep asking the user questions until the user enters "n" or "N"
-        while (moreQuestions) {
-            System.out.print("Enter your question: ");
-            question = in.nextLine();
-            answer = answers[rnd.nextInt(20)];
-            System.out.println("YOU ASKED: " + question);
-            System.out.println("MAGIC 8-BALL SAYS: " + answer);
-            System.out.print("Do you have another question for the Magic 8-Ball? (y/n) ");
-            reply = in.nextLine();
-            if (reply.equals("n") || reply.equals ("N")) moreQuestions = false;
-        }
-
-        // Close out the scanner to release resources
-        in.close();
     }
 
 }
